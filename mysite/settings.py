352302ebfd55c41351cfe06df734130f8003c3ff
marlonvess <marlonvess@gmail.com>
2026-01-14 21:46:27 +0000

    bug fix on .env

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-load_dotenv()
+load_dotenv(BASE_DIR / '.env')
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
