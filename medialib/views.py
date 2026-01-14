a390bc5a24a75a7fae9f88dd2a31316889e8bfb8
marlonvess <marlonvess@gmail.com>
2026-01-14 14:56:10 +0000

    Basic template to show posts from db

--- medialib/views.py
+++ medialib/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import MediaItem
 
 # Create your views here.
+
+def post_list(request):
+    media_items = MediaItem.objects.filter(post_date__lte=timezone.now()).order_by('post_date')
+    return render(request, 'medialib/post_list.html', {'media_items': media_items})
