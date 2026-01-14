9f02dbb79047bffccfcb1e7380220e16a14a12b2
marlonvess <marlonvess@gmail.com>
2026-01-14 17:56:02 +0000

    added templates and post detail and some css

--- medialib/views.py
+++ medialib/views.py
@@ -1,9 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import MediaItem
+
 
 # Create your views here.
 
 def post_list(request):
     media_items = MediaItem.objects.filter(post_date__lte=timezone.now()).order_by('post_date')
     return render(request, 'medialib/post_list.html', {'media_items': media_items})
+
+def post_detail(request, pk):
+    post = get_object_or_404(MediaItem, pk=pk)
+    return render(request, 'medialib/post_detail.html', {'post': post})
